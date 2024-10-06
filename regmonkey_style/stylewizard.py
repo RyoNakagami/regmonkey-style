30e6ae83cae829bce98f2d49f4e10fb20deaff19
RyoNak <nakagamiryo0901@gmail.com>
2024-10-06 23:32:22 +0900

    ENH: adding matplotlib format

--- regmonkey_style/stylewizard.py
+++ regmonkey_style/stylewizard.py
@@ -1,13 +1,11 @@
 # %%
 from regmonkey_style.config import CONFIG
 import plotly.io as pio
+import matplotlib.pyplot as plt
 import regmonkey_style.common.setup_custom_font as scf
 import regmonkey_style.plotly_custom.templates as pct
+import regmonkey_style.matplotlib_custom.templates as mct
 from regmonkey_style.common import utils
-
-
-def set_default():
-    scf.add_custom_font(CONFIG.font_style.default_font)
 
 
 def show_available_fonts():
@@ -18,24 +16,76 @@ def show_available_templates():
     return CONFIG.templates
 
 
-def set_font(font):
+def set_font(font=CONFIG.font_style.default_font):
     if font not in CONFIG.font_style.available_list:
         raise ValueError("{} is not in templates list".format(font))
+    scf.add_custom_font(font)
 
 
 def set_templates(template):
-    if template not in CONFIG.templates:
-        raise ValueError("template is not in templates list")
+    """
+    Set the Plotly/Matplotlib template for the application.
 
-    Template_instance = pct.Templates
-    custom_template = getattr(Template_instance, template)()
-    pio.templates[template] = custom_template
+    Args:
+        template (str): The name of the template to set.
+
+    Raises:
+        ValueError: If the specified template is not in the list of available templates.
+    """
+    if template not in CONFIG.templates:
+        raise ValueError(f"{template} is not in the templates list")
+
+    # Set Plotly template
+    set_plotly_template(template)
+
+    # Set Matplotlib template
+    set_matplotlib_template(template)
+
+
+def set_plotly_template(template):
+    """
+    Set the Plotly template for the application.
+
+    Args:
+        template (str): The name of the template to set.
+    """
+    plotly_template_instance = pct.Templates
+    plotly_custom_template = getattr(plotly_template_instance, template)()
+    pio.templates[template] = plotly_custom_template
     pio.templates.default = template
 
 
+def set_matplotlib_template(template):
+    """
+    Set the Matplotlib template for the application.
+
+    Args:
+        template (str): The name of the template to set.
+    """
+    matplotlib_template_instance = mct.Templates
+    matplotlib_custom_template = getattr(matplotlib_template_instance, template)()
+    plt.rcParams.update(matplotlib_custom_template)
+
+
 def equal_xy_scale(figure_object):
+    """
+    Ensure equal scaling for the x and y axes of a Plotly figure.
+
+    Args:
+        figure_object: The Plotly or Matplotlib figure object to modify.
+
+    Returns:
+        figure_object: The modified Plotly figure object with equal x and y scaling.
+    """
     return utils.equal_xy_scale(figure_object)
 
 
 def restore_default():
+    """
+    Restore the default Plotly template and Matplotlib settings.
+
+    This function resets the Plotly template to the default "plotly" template
+    and restores the default Matplotlib settings.
+    """
     pio.templates.default = "plotly"
+    plt.rcdefaults()
